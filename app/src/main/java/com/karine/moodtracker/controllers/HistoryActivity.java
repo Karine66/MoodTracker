d7a36a70b9b67d7988aff7b1823c84fc51c1dda5
Karine66 <kdbt1973>
2019-05-25 21:43:43 +0200

    date comparate ok with yesterday

--- app/src/main/java/com/karine/moodtracker/controllers/HistoryActivity.java
+++ app/src/main/java/com/karine/moodtracker/controllers/HistoryActivity.java
@@ -9,13 +9,12 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.gson.Gson;
 import com.karine.moodtracker.R;
-import com.karine.moodtracker.models.AlertDialog;
 import com.karine.moodtracker.models.Mood;
 
-import java.util.Objects;
+import java.util.Calendar;
+import java.util.Date;
 
 import static android.view.View.*;
 
@@ -23,12 +22,12 @@ import static android.view.View.*;
 public class HistoryActivity extends AppCompatActivity {
 
 
+    private static Object Date;
     private SharedPreferences mPreferences;
     private ImageView mHistorybtn1;
     private TextView mTvYesterday;
     private SharedPreferences myPrefs;
     private View mYesterday;
-    private MainActivity mContext;
 
 
     @Override
@@ -39,7 +38,7 @@ public class HistoryActivity extends AppCompatActivity {
 
         mTvYesterday = findViewById(R.id.tvYesterday);
         mHistorybtn1 = findViewById(R.id.history_btn_1);
-        mYesterday  = findViewById(R.id.yesterday);
+        mYesterday = findViewById(R.id.yesterday);
 
         retrieveComment();
         retrieveBackground();
@@ -47,9 +46,9 @@ public class HistoryActivity extends AppCompatActivity {
     }
 
     private void retrieveComment() {
-        
+
         mPreferences = getSharedPreferences("saved", Context.MODE_PRIVATE);
-        if ( mPreferences.getString("saved","").isEmpty()) {
+        if (mPreferences.getString("saved", "").isEmpty()) {
             mHistorybtn1.setVisibility(INVISIBLE);
         } else {
             mHistorybtn1.setVisibility(VISIBLE);
@@ -71,23 +70,112 @@ public class HistoryActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences = getSharedPreferences("save_bg", Context.MODE_PRIVATE);
         String json = sharedPreferences.getString("save_bg", "");
         Gson gson = new Gson();
-        Mood mood = gson.fromJson(json,  Mood.class);
+        Mood mood = gson.fromJson(json, Mood.class);
 
-            Log.d("Test_bg", "color" +  mood.getSelectedMood());
+        Log.d("Test_bg", "color" + mood.getSelectedMood());
 
-           mYesterday.setBackgroundResource(Mood.ARRAY_BACKGROUND_COLOR[mood.getSelectedMood()]);
+        mYesterday.setBackgroundResource(Mood.ARRAY_BACKGROUND_COLOR[mood.getSelectedMood()]);
 
     }
 
 
-        public void retrieveDate() {
+    public void retrieveDate() {
+
+
         myPrefs = getSharedPreferences("save_date", Context.MODE_PRIVATE);
         String date = myPrefs.getString("save_date", "");
         Log.d("Test_Date", "onCreate() called with" + date);
 
+        compareDate();
+        Log.d("Test_compareDate", "Yesterday was" + compareDate() );
         mTvYesterday.setText(date);
     }
+        public java.util.Date compareDate() {
+
+
+            Calendar toDayCalendar = Calendar.getInstance();
+            Date date1 = toDayCalendar.getTime();
+
+
+            Calendar yesterdayCalendar = Calendar.getInstance();
+            yesterdayCalendar.add(Calendar.DAY_OF_MONTH, -1);
+            Date date2 = yesterdayCalendar.getTime();
+
+
+            if (date1.compareTo(date2) < -1) {
+
+
+            }
+            return date2;
+        }
 }
+
+//    private String compare (String stringData, String yesterday) {
+//        String result = "";
+//
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//        Date dateMonth = null;
+//
+//        try {
+//            Date = simpleDateFormat.parse(stringData);
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+//        Long millisSecond = convertToMillisSecond(dateMonth);
+//        Long currencyMillisSecond = System.currentTimeMillis();
+//
+//        if (currencyMillisSecond > millisSecond) {
+//            Long diff = currencyMillisSecond - millisSecond;
+//            Long day = 86400000L;
+//
+//            if (diff < day && getCurrentDayOfMonth() == getDateDayOfMonth(dateMonth)) {
+//                result = convertMillisSecondsToDateString(millisSecond);
+//            } else if (diff < (day * 2) && getCurrentDayOfMonth() - 1 == getDateDayOfMonth(dateMonth)) {
+//                result = yesterday;
+//
+//            } else {
+//                result = convertMillisSecondsToDateString(millisSecond);
+//
+//            }
+//        }
+//        Log.d("Test_Yesterday", "Yesterday was" + result);
+//        return result;
+//
+//
+//    }
+//
+//
+//    private static int getDateDayOfMonth(Date dateMonth) {
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.setTime(dateMonth);
+//        return calendar.get(Calendar.DAY_OF_MONTH);
+//    }
+//
+//    public static int getCurrentDayOfMonth() {
+//        Calendar calendar = Calendar.getInstance();
+//        return calendar.get(Calendar.DAY_OF_MONTH);
+//    }
+//
+//    public static String convertMillisSecondsToDateString(long millisSecond) {
+//        Date dateMonth = new Date(millisSecond);
+//        Format formatter = new SimpleDateFormat("dd / MM / yyyy");
+//        return formatter.format(dateMonth);
+//    }
+//
+//    public static long convertToMillisSecond(Date dateMonth) {
+//        return dateMonth.getTime();
+//    }
+
+
+
+
+
+
+
+
+
+
 
 
 
