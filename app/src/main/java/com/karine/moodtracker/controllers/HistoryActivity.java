05e4beaa1a2cbf684976ef8cfe080234157aa99b
Karine66 <kdbt1973>
2019-06-05 19:39:08 +0200

    change "textView"

--- app/src/main/java/com/karine/moodtracker/controllers/HistoryActivity.java
+++ app/src/main/java/com/karine/moodtracker/controllers/HistoryActivity.java
@@ -1,11 +1,9 @@
 package com.karine.moodtracker.controllers;
 
-import android.annotation.SuppressLint;
+
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -15,49 +13,30 @@ import android.widget.Toast;
 import com.google.gson.Gson;
 import com.karine.moodtracker.R;
 import com.karine.moodtracker.models.Mood;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
-
 import static android.view.View.*;
-import static com.karine.moodtracker.models.Mood.*;
 import static com.karine.moodtracker.models.Mood.ARRAY_BACKGROUND_COLOR;
 
 
 public class HistoryActivity extends AppCompatActivity {
 
-
-
     private SharedPreferences mPreferences;
     private ImageView mHistorybtn1;
-
-    //Initialize TextView
-    private TextView mTvYesterday;
-    private TextView mTvDays2Ago;
-    private TextView mTvDays3Ago;
-    private TextView mTvDays4Ago;
-    private TextView mTvDays5Ago;
-    private TextView mTvDays6Ago;
-    private TextView mTvDays7Ago;
-    //View
-    private View mYesterday;
-    private View mDays2Ago;
-
-
-    private long dayAgoResult;
-
+    private TextView mTvpastDays;
+      private long dayAgoResult;
     private SharedPreferences myPrefs;
-
-
     private String date;
     private String dayDate;
-
     private long diff;
     private long daysBetween;
+
+    private View mDays2Ago;
+    private View mYesterday;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,13 +44,13 @@ public class HistoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_history);
 
         //TextView
-        mTvYesterday = findViewById(R.id.tvYesterday);
-        mTvDays2Ago = findViewById(R.id.tvDays2Ago);
-        mTvDays3Ago = findViewById(R.id.tvDays3Ago);
-        mTvDays4Ago = findViewById(R.id.tvDays4Ago);
-        mTvDays5Ago = findViewById(R.id.tvDays5Ago);
-        mTvDays6Ago = findViewById(R.id.tvDays6Ago);
-        mTvDays7Ago = findViewById(R.id.tvDays7Ago);
+        mTvpastDays = findViewById(R.id.tvYesterday);
+        mTvpastDays = findViewById(R.id.tvDays2Ago);
+        mTvpastDays = findViewById(R.id.tvDays3Ago);
+        mTvpastDays = findViewById(R.id.tvDays4Ago);
+        mTvpastDays = findViewById(R.id.tvDays5Ago);
+        mTvpastDays = findViewById(R.id.tvDays6Ago);
+        mTvpastDays = findViewById(R.id.tvDays7Ago);
 
         mHistorybtn1 = findViewById(R.id.history_btn_1);
         //View
@@ -81,7 +60,6 @@ public class HistoryActivity extends AppCompatActivity {
         retrieveComment();
         retrieveBackground();
         retrieveDate();
-
 
     }
 
@@ -116,10 +94,7 @@ public class HistoryActivity extends AppCompatActivity {
 
        mYesterday.setBackgroundResource(ARRAY_BACKGROUND_COLOR[mood.getSelectedMood()]);
 
-
     }
-
-
 
     public void retrieveDate() {
 
@@ -132,19 +107,13 @@ public class HistoryActivity extends AppCompatActivity {
         SimpleDateFormat jsonDateFormat = new SimpleDateFormat("dd/MM/yyyy");
         String dayDate = jsonDateFormat.format(mCalendar.getTime());
 
-        olderDays(date, dayDate);
-        //daysBetween(date, dayDate);
-        //Log.d("Test_Compare", "Yesterday was" + dayAgoResult);
-//
-        //mTvYesterday.setText(dat
+        olderDays(date, dayDate, mTvpastDays);
+
+        Log.d("Test_Compare", "Yesterday was" + dayAgoResult);
 
     }
 
-
-
     public long daysBetween (String day1, String day2) {
-
-
 
         long daysBetween = 0;
         SimpleDateFormat myFormat = new SimpleDateFormat("dd/MM/yyyy");
@@ -155,85 +124,47 @@ public class HistoryActivity extends AppCompatActivity {
 
             long diff = date2.getTime() - date1.getTime();
 
-
-
             daysBetween = (TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
 
-            //Log.d("Test_Between", "diff" + diff);
-            //Log.d("Test_yesterday","Il ya a"+daysBetween+"jours");
+            Log.d("Test_Between", "diff" + diff);
+
         } catch (ParseException e) {
             e.printStackTrace();
-
-
         }
 
         return daysBetween;
 
     }
 
+    private long olderDays (String date, String dayDate, TextView textView) {
 
-
-    private long olderDays (String date, String dayDate) {
-
-
-       dayAgoResult= daysBetween(date, dayDate);
-        dayAgoResult=1;
-
-
+        dayAgoResult= daysBetween(date, dayDate);
+        dayAgoResult=2;
 
         switch ((int) dayAgoResult) {
             case 1 :
 
+                textView.setText("Hier");
 
-                    mTvYesterday.setText("Il y a" + dayAgoResult + "jours");
-
-                Log.d("Test_Days", "Il y a" + dayAgoResult+ "jours");
+                Log.d("Test_Days", "Hier");
 
             break;
 
             case 2:
 
+                textView.setText("Avant-hier");
 
-                    mTvDays2Ago.setText("Il y a" + dayAgoResult + "jours");
-
-                Log.d("Test_2Days", "Il y a 2 jours" + dayAgoResult);
             break;
 
-            case 3:
+            case 3: case 4: case 5: case 6:
 
-                mTvDays3Ago.setText("Il y a 3 jours");
+               textView.setText("Il y a " + dayAgoResult + "jours");
 
-                Log.d("Test_3Days", "Il y a 3 jours" + dayAgoResult);
-            break;
-
-            case 4:
-
-                mTvDays4Ago.setText("Il y a 4 jours");
-
-                Log.d("Test_4Days", "Il y a 4 jours" + dayAgoResult);
-            break;
-
-            case 5:
-
-                mTvDays5Ago.setText("Il y a 5 jours");
-
-                Log.d("Test_5Days", "Il y a 5 jours" + dayAgoResult);
-            break;
-
-            case 6:
-
-                mTvDays6Ago.setText("Il y a 6 jours");
-
-                Log.d("Test_6Days", "Il y a 6 jours" + dayAgoResult);
             break;
 
             case 7:
 
-                mTvDays7Ago.setText("Il y a 7 jours");
-
-                Log.d("Test_7Days", "Il y a 7 jours" + dayAgoResult);
-            break;
-
+                textView.setText("Il ya " + dayAgoResult + "jours");
         }
 
         return dayAgoResult;
